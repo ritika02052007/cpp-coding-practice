95e6c9db1d21d1f3f1caf1272022c46bab61bb5c
ritika02052007 <ritika20070205@gmail.com>
2026-06-18 09:57:52 +0530

    my cpp practice journey

---
+++ PracticeQuestion/25_invertesdPyramid.cpp
@@ -0,0 +1,17 @@
+// wap to print the inverse pyramid
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the number of rows and column :";
+    cin>>n;
+    for(int i=n;i>=1; i--){
+        for(int j=1;j<=i;j++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
