499e6ce834fa50a418cc3b5aafb60c62503b67e2
ritika02052007 <ritika20070205@gmail.com>
2026-06-18 09:54:04 +0530

    my cpp practice journey

---
+++ PracticeQuestion/24_halfPyramidNumber.cpp
@@ -0,0 +1,16 @@
+// wap to print half pyramid using the number
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the number of rows and column :";
+    cin>>n;
+    for(int i = 1; i<=n; i++){
+        for(int j=1;j<=i; j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
