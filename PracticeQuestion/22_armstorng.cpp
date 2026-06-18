8755169fe8ad319af145ce082fa0899a05832a15
ritika02052007 <ritika20070205@gmail.com>
2026-06-18 09:45:00 +0530

    my cpp practice journey

---
+++ PracticeQuestion/22_armstorng.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,lastDigit = 0,arm =0;
+    cout<<"enter the number :";
+    cin>>n;
+    int armstrong = n;
+    while(n>0){
+      lastDigit = n%10;
+      arm = (lastDigit*lastDigit*lastDigit)+arm;
+      n/=10; 
+    }
+    if(arm == armstrong){
+        cout<<"it is a armstrong number";
+    }  else {
+        cout<<"it is not a armstorng number";
+    }
+    return 0;
+}
