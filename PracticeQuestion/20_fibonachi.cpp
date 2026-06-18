370a51db08352a4478a8cf2da8b63459790e2fd8
ritika02052007 <ritika20070205@gmail.com>
2026-06-18 09:30:28 +0530

    my cpp practice journey

---
+++ PracticeQuestion/20_fibonachi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, a = 0, b = 1, c;
+
+    cout << "enter the number of terms ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        cout << a << " ";
+
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return 0;
+}
